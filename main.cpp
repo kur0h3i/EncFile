5bd16fccafed7760d19757cdc30d97fd6e48fde3
Pedro Perez <pedropm.lolo05@gmail.com>
2025-10-06 08:04:11 +0200

    Interfaz | Menu | Auth

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@
 int main() {
   int opcion = 0;
   User user("", "");  // Usuario vacío inicial
+  bool autenticado = false;
 
   do {
-    std::cout << "1. Registrarse" << '\n'
-              << "2. Iniciar Sesion" << '\n'
-              << "3. Salir" << '\n'
-              << ">> ";
-    std::cin >> opcion;
+    cout << "1. Registrarse" << '\n'
+         << "2. Iniciar Sesion" << '\n'
+         << "3. Salir" << '\n'
+         << ">> ";
+    cin >> opcion;
 
+    // Panel Autenticacion
     switch (opcion) {
       case 1: {
         // Registro: crea y guarda el usuario
@@ -21,25 +23,65 @@ int main() {
       }
       case 2: {
         // Login: pedir credenciales y verificar
-        std::string nombre, contrasena;
-        std::cout << "Usuario: ";
-        std::cin >> nombre;
-        std::cout << "Contrasena: ";
-        std::cin >> contrasena;
+        string nombre, contrasena;
+        cout << "Usuario: ";
+        cin >> nombre;
+        cout << "Contrasena: ";
+        cin >> contrasena;
         try {
           user = user.iniciarSesion(nombre, contrasena);
-          std::cout << "Login OK\n";
-        } catch (const std::exception& e) {
-          std::cout << e.what() << "\n";
+          autenticado = true;
+          cout << "Login OK\n";
+        } catch (exception& e) {
+          cout << e.what() << "\n";
         }
         break;
       }
       case 3:
-        std::cout << "Saliendo...\n";
+        cout << "Saliendo...\n";
         break;
       default:
-        std::cout << "Opcion no valida\n";
+        cout << "Opcion no valida\n";
         break;
+    }
+
+    // Autenticad
+    while (autenticado) {
+      int subOpcion = 0;
+      cout << "--------------------\n"
+           << "Usuario : " << user.getNombre() << '\n'
+           << "------- Menú -------\n"
+           << "1. Cifrar Archivo\n"
+           << "2. Descifrar archivo\n"
+           << "3. Listar archivos cifrados\n"
+           << "4. Eliminar archivo\n"
+           << "5. Cerrar sesión\n"
+           << ">> ";
+      cin >> subOpcion;
+
+      // Menu
+      switch (subOpcion) {
+        case 1:
+          // Cifrar Archivos
+          break;
+        case 2:
+          // Descifrar Archivos
+          break;
+        case 3:
+          // Listar Archivos Cifrados
+          break;
+        case 4:
+          // Eliminar Archivo
+          break;
+        case 5:
+          // Cerrar Sesion
+          autenticado = false;
+          cout << "Sesión cerrada.\n";
+          break;
+        default:
+          cout << "Opción no válida.\n";
+          break;
+      }
     }
   } while (opcion != 3);
 
